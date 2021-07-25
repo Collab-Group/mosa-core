65b3e1e0d4eaf0d18f960a86b2d3fc175f1f8d87
nifanfa <nifanfa@foxmail.com>
2021-07-26 07:23:26 +0800

    Update GraphicsSelector.cs

--- Source/Mosa.External.x86/Drawing/GraphicsSelector.cs
+++ Source/Mosa.External.x86/Drawing/GraphicsSelector.cs
@@ -7,19 +7,19 @@ namespace Mosa.External.x86.Drawing
     {
         public static Graphics GetGraphics(int width = 640, int height = 480)
         {
-            // Even if VBE is enabled, choose VMWare SVGA II driver first for best performance in VM
+            // VBE First
+            if (VBE.IsVBEAvailable)
+            {
+                return new VBEGraphics();
+            }
+
             if (PCI.Exists(VendorID.VMWare, DeviceID.SVGAIIAdapter))
             {
                 return new VMWareSVGAIIGraphics(width, height);
             }
-            else if (VBE.IsVBEAvailable)
-            {
-                return new VBEGraphics();
-            }
-            else
-            {
-                throw new System.Exception("No graphics are available for the current system.");
-            }
+
+            Panic.Error("No graphics are available for the current system.");
+            return null;
         }
     }
 }
