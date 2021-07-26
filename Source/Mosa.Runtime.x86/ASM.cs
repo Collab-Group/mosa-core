4788b8b573d645c0ed2d8d1ff9915cae61832ccc
nifanfa <nifanfa@foxmail.com>
2021-07-27 01:22:29 +0800

    Create ASM.cs

---
+++ Source/Mosa.Runtime.x86/ASM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Mosa.Runtime.x86
+{
+    public static class ASM
+    {
+        [DllImport("MEMCPY.o", EntryPoint = "MEMCPY")]
+        public static extern void MEMCPY(uint DEST, uint SOURCE, uint LENGTH);
+    }
+}
