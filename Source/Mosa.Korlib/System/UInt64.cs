e059af27411308801b7e88220c79b48f094bae5f
nifanfa <nifanfa@foxmail.com>
2021-07-27 00:33:36 +0800

    UInt64ToString Impl

--- Source/Mosa.Korlib/System/UInt64.cs
+++ Source/Mosa.Korlib/System/UInt64.cs
@@ -2,58 +2,59 @@
 
 namespace System
 {
-	/// <summary>
-	///
-	/// </summary>
-	public struct UInt64
-	{
-		public const ulong MaxValue = 0xffffffffffffffff;
-		public const ulong MinValue = 0;
+    /// <summary>
+    ///
+    /// </summary>
+    public struct UInt64
+    {
+        public const ulong MaxValue = 0xffffffffffffffff;
+        public const ulong MinValue = 0;
 
-		internal ulong _value;
+        internal ulong _value;
 
-		public int CompareTo(ulong value)
-		{
-			if (_value < value) return -1;
-			else if (_value > value) return 1;
-			return 0;
-		}
-
-		public bool Equals(ulong obj)
-		{
-			return Equals((object)obj);
-		}
-
-		public override bool Equals(object obj)
-		{
-			return ((ulong)obj) == _value;
-		}
-
-		public override int GetHashCode()
-		{
-			return (int)_value;
-		}
-
-		//Not Available Until GC Is Setup
-        public override string ToString()
+        public int CompareTo(ulong value)
         {
-			string s = "";
-			ulong temp = this;
+            if (_value < value) return -1;
+            else if (_value > value) return 1;
+            return 0;
+        }
 
-			do
-			{
-				s += (char)((temp % 10) + 0x30);
-				temp /= 10;
-			} while (temp != 0);
+        public bool Equals(ulong obj)
+        {
+            return Equals((object)obj);
+        }
 
-			string r = "";
+        public override bool Equals(object obj)
+        {
+            return ((ulong)obj) == _value;
+        }
 
-			for (int i = 0; i < s.Length; i++)
-			{
-				r += s[s.Length - 1 - i];
-			}
+        public override int GetHashCode()
+        {
+            return (int)_value;
+        }
 
-			return r;
-		}
+        //Not Available Until GC Is Setup
+        public override unsafe string ToString()
+        {
+            int count = 0;
+            ulong tmp = this;
+            do
+            {
+                tmp /= 10;
+                count++;
+            } while (tmp != 0);
+
+            string s = String.InternalAllocateString(count);
+            ulong temp = this;
+
+            for (int i = count - 1; i >= 0; i--)
+            {
+                s.first_char[i] = (char)((temp % 10) + 0x30);
+                temp /= 10;
+            }
+
+            return s;
+        }
     }
 }
