573c306baa51e411361ef5bb6212729835884180
AnErrupTion <ichicharka@gmail.com>
2021-07-28 14:00:28 +0200

    Add IDisk.WriteBlock() and IDEDisk.WriteBlock()

--- Source/Mosa.External.x86/FileSystem/IDEDisk.cs
+++ Source/Mosa.External.x86/FileSystem/IDEDisk.cs
@@ -1,11 +1,10 @@
 ﻿using Mosa.External.x86.Driver;
-using System;
 
 namespace Mosa.External.x86.FileSystem
 {
     public class IDEDisk : IDisk
     {
-        IDE IDE;
+        private readonly IDE IDE;
 
         public IDEDisk()
         {
@@ -16,6 +15,11 @@ namespace Mosa.External.x86.FileSystem
         public bool ReadBlock(uint sector, uint count, byte[] data)
         {
             return IDE.ReadBlock(IDE.Drive.Drive0, sector, count, data);
+        }
+
+        public bool WriteBlock(uint sector, uint count, byte[] data)
+        {
+            return IDE.WriteBlock(IDE.Drive.Drive0, sector, count, data);
         }
     }
 }
