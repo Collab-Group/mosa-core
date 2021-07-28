07b18597193b59963c1092a77ab9bb977b14800e
AnErrupTion <ichicharka@gmail.com>
2021-07-28 03:43:10 +0200

    Revert "Remove MemoryBlock.Fill32() (replaced by ASM.MEMFILL)"
    This reverts commit 4f960bdb65c0e0e2f3551381d2d47b69a9eab5bb.

--- Source/Mosa.External.x86/FileSystem/FAT12.cs
+++ Source/Mosa.External.x86/FileSystem/FAT12.cs
@@ -1,10 +1,13 @@
-﻿using Mosa.External.x86.Driver;
+﻿using Mosa.External.x86;
+using Mosa.External.x86.Driver;
 using Mosa.External.x86.Encoding;
+using Mosa.External.x86.FileSystem;
 using Mosa.Kernel.x86;
 using Mosa.Runtime;
 using System.Collections.Generic;
 
 namespace Mosa.External.x86.FileSystem
+//namespace MOSA1
 {
     public class FAT12
     {
@@ -27,36 +30,47 @@ namespace Mosa.External.x86.FileSystem
             public string ParentPath;
         }
 
-        public IDisk Disk;
+        public List<FileInfo> FileInfos;
+        IDisk Disk;
 
-        public uint fileListSectorLength;
-        public uint fileListSector0ffset;
-        public uint fileAreaSectorOffset;
-        public PartitionInfo partitionInfo;
+        uint fileListSectorLength;
+        uint fileListSector0ffset;
+        uint fileAreaSectorOffset;
+        PartitionInfo partitionInfo;
 
         FAT12Header fAT12Header;
 
         public FAT12(IDisk disk, PartitionInfo _partitionInfo)
         {
             Disk = disk;
+            FileInfos = new List<FileInfo>();
             partitionInfo = _partitionInfo;
+
 
             byte[] header = new byte[IDE.SectorSize];
             disk.ReadBlock(partitionInfo.LBA, 1, header);
 
             MemoryBlock memoryBlock = new MemoryBlock(header);
 
+
             fAT12Header = new FAT12Header() { OEMName = "" };
             for (int i = 0; i < 8; i++)
+            {
                 fAT12Header.OEMName += ASCII.GetChar(memoryBlock.Read8((uint)(0x3 + i)));
+            }
 
             fAT12Header.SectorsPerCluster = memoryBlock.Read8(0xD);
+
             fAT12Header.RootEntryCount = memoryBlock.Read16(0x11);
+
             fAT12Header.NumberOfFATs = memoryBlock.Read8(0x10);
+
             fAT12Header.SectorsPerFATs = memoryBlock.Read16(0x16);
+
             fAT12Header.ResvdSector = memoryBlock.Read16(0x0e);
 
-            fileListSectorLength = (uint)((fAT12Header.RootEntryCount * 32 + (IDE.SectorSize - 1)) / IDE.SectorSize);
+
+            this.fileListSectorLength = (uint)((fAT12Header.RootEntryCount * 32 + (IDE.SectorSize - 1)) / IDE.SectorSize);
 
             /*
              * |    Boot   |
@@ -64,44 +78,11 @@ namespace Mosa.External.x86.FileSystem
              * |    FAT2   |
              * | Directory |
              */
-            fileListSector0ffset = (uint)(partitionInfo.LBA + fAT12Header.ResvdSector + fAT12Header.SectorsPerFATs * 2);
+            this.fileListSector0ffset = (uint)(partitionInfo.LBA + fAT12Header.ResvdSector + fAT12Header.SectorsPerFATs * 2);
 
-            fileAreaSectorOffset = (fAT12Header.ResvdSector + ((uint)fAT12Header.NumberOfFATs * fAT12Header.SectorsPerFATs) + ((fAT12Header.RootEntryCount * 32u) / IDE.SectorSize));
+            this.fileAreaSectorOffset = (fAT12Header.ResvdSector + ((uint)fAT12Header.NumberOfFATs * fAT12Header.SectorsPerFATs) + ((fAT12Header.RootEntryCount * 32u) / IDE.SectorSize));
 
-            GetFileList(@"/");
-        }
-
-        public FileInfo Create(string Name, bool IsDirectory)
-        {
-            Name = Name.ToUpper();
-
-            string Path;
-            string FileName;
-
-            if (Name.IndexOf('/') == -1)
-            {
-                Path = "/";
-                FileName = Name;
-            }
-            else
-            {
-                Path = Name.Substring(0, Name.LastIndexOf('/') + 1);
-                FileName = Name.Substring(Path.Length);
-            }
-
-            List<FileInfo> fileInfos = GetFileList(Path);
-            FileInfo lastFileInfo = fileInfos[fileInfos.Count - 1];
-
-            lastFileInfo.Name = FileName;
-            lastFileInfo.ParentPath = Path;
-            lastFileInfo.Size = 0;
-            lastFileInfo.IsDirectory = IsDirectory;
-
-            ushort offset = (ushort)(partitionInfo.LBA + fileAreaSectorOffset + ((lastFileInfo.Cluster - 2) * fAT12Header.SectorsPerCluster));
-
-            lastFileInfo.Cluster = offset;
-
-            return lastFileInfo;
+            ReadFileList(fileListSector0ffset, @"/");
         }
 
         public byte[] ReadAllBytes(string Name)
@@ -122,138 +103,56 @@ namespace Mosa.External.x86.FileSystem
                 FileName = Name.Substring(Path.Length);
             }
 
-            // We could use Exists() here but for optimization sake we aren't
-            FileInfo fileInfo = new FileInfo() { Name = "" };
-            foreach (var v in GetFileList(Path))
+            FileInfo fileInfo = new FileInfo() { Size = 0 };
+            foreach (var v in FileInfos)
+            {
                 if (v.Name == FileName && v.ParentPath == Path)
                 {
                     fileInfo = v;
-                    break;
                 }
+            }
+            if (fileInfo.Size == 0)
+            {
+                Panic.Error("No such file");
+            }
 
-            if (fileInfo.Name == "")
-                Panic.Error("No such file: " + Name.ToLower());
+            uint count = 0;
+            if (fileInfo.Size <= IDE.SectorSize)
+            {
+                count = 1;
+            }
+            else
+            {
+                if (fileInfo.Size % IDE.SectorSize != 0)
+                {
+                    count = fileInfo.Size / IDE.SectorSize + 1;
+                }
+                else
+                {
+                    count = fileInfo.Size / IDE.SectorSize;
+                }
+            }
 
-            uint count = fileInfo.Size <= IDE.SectorSize ? 1 : (fileInfo.Size / IDE.SectorSize + (fileInfo.Size % IDE.SectorSize != 0 ? 1 : (uint)0));
             uint offset = (uint)(partitionInfo.LBA + fileAreaSectorOffset + ((fileInfo.Cluster - 2) * fAT12Header.SectorsPerCluster));
 
+            // TODO: Maybe make ReadBlock() only read count?
             byte[] data = new byte[IDE.SectorSize * count];
             Disk.ReadBlock(offset, count, data);
 
             byte[] result = new byte[fileInfo.Size];
+
             for (int i = 0; i < fileInfo.Size; i++)
+            {
                 result[i] = data[i];
+            }
 
             GC.DisposeObject(data);
 
             return result;
         }
 
-        public void WriteAllBytes(string Name, byte[] Data)
+        public void ReadFileList(uint startSector, string parentPath)
         {
-            Name = Name.ToUpper();
-
-            string Path;
-            string FileName;
-
-            if (Name.IndexOf('/') == -1)
-            {
-                Path = "/";
-                FileName = Name;
-            }
-            else
-            {
-                Path = Name.Substring(0, Name.LastIndexOf('/') + 1);
-                FileName = Name.Substring(Path.Length);
-            }
-
-            FileInfo fileInfo = new FileInfo() { Name = "" };
-            foreach (var v in GetFileList(Path))
-                if (v.Name == FileName && v.ParentPath == Path)
-                {
-                    fileInfo = v;
-                    break;
-                }
-
-            if (fileInfo.Name == "")
-                fileInfo = Create(Name, false);
-
-            uint count = fileInfo.Size <= IDE.SectorSize ? 1 : (fileInfo.Size / IDE.SectorSize + (fileInfo.Size % IDE.SectorSize != 0 ? 1 : (uint)0));
-            uint offset = (uint)(partitionInfo.LBA + fileAreaSectorOffset + ((fileInfo.Cluster - 2) * fAT12Header.SectorsPerCluster));
-
-            Disk.WriteBlock(offset, count, Data);
-        }
-
-        public List<FileInfo> GetFileList(string parentPath)
-        {
-            return ReadFileList(fileListSector0ffset, parentPath);
-        }
-
-        public FileInfo GetFileInfo(string Name)
-        {
-            Name = Name.ToUpper();
-
-            string Path;
-            string FileName;
-
-            if (Name.IndexOf('/') == -1)
-            {
-                Path = "/";
-                FileName = Name;
-            }
-            else
-            {
-                Path = Name.Substring(0, Name.LastIndexOf('/') + 1);
-                FileName = Name.Substring(Path.Length);
-            }
-
-            FileInfo fileInfo = new FileInfo() { Name = "" };
-            foreach (var v in GetFileList(Path))
-                if (v.Name == FileName && v.ParentPath == Path)
-                {
-                    fileInfo = v;
-                    break;
-                }
-
-            if (fileInfo.Name == "")
-                Panic.Error("No such file: " + Name.ToLower());
-
-            return fileInfo;
-        }
-
-        public bool Exists(string Name)
-        {
-            Name = Name.ToUpper();
-
-            string Path;
-            string FileName;
-
-            if (Name.IndexOf('/') == -1)
-            {
-                Path = "/";
-                FileName = Name;
-            }
-            else
-            {
-                Path = Name.Substring(0, Name.LastIndexOf('/') + 1);
-                FileName = Name.Substring(Path.Length);
-            }
-
-            FileInfo fileInfo = new FileInfo() { Name = "" };
-            foreach (var v in GetFileList(Path))
-                if (v.Name == FileName && v.ParentPath == Path)
-                {
-                    fileInfo = v;
-                    break;
-                }
-
-            return fileInfo.Name != "";
-        }
-
-        private List<FileInfo> ReadFileList(uint startSector, string parentPath)
-        {
-            List<FileInfo> FileInfos = new List<FileInfo>();
-
             byte[] data = new byte[fileListSectorLength * IDE.SectorSize];
             Disk.ReadBlock(startSector, fileListSectorLength, data);
 
@@ -302,8 +201,6 @@ namespace Mosa.External.x86.FileSystem
 
                 //GC.DisposeObject(fileInfo);
             }
-
-            return FileInfos;
         }
 
         private FileInfo GetFileInfo(byte[] _data, string parentPath)
--- Source/Mosa.External.x86/FileSystem/MBR.cs
+++ Source/Mosa.External.x86/FileSystem/MBR.cs
@@ -1,4 +1,7 @@
-﻿using Mosa.Kernel.x86;
+﻿using Mosa.External.x86;
+using Mosa.External.x86.FileSystem;
+using Mosa.Kernel.x86;
+using System;
 using System.Collections.Generic;
 
 namespace Mosa.External.x86.FileSystem
@@ -19,7 +22,6 @@ namespace Mosa.External.x86.FileSystem
         {
             byte[] mbrData = new byte[512];
             disk.ReadBlock(0, 1, mbrData);
-
             memoryBlock = new MemoryBlock(mbrData);
             PartitionInfos = new List<PartitionInfo>();
 
@@ -28,15 +30,16 @@ namespace Mosa.External.x86.FileSystem
 
         public static void LoadPartitionInfo() 
         {
-            for (int i = 0x1BE; i < 0x1FE; i += 16) 
+            for(int i  = 0x1BE;i< 0x1FE; i += 16) 
             {
                 bool _IsBootable = memoryBlock.Read8((uint)(i + 0)) == 0x80;
-
                 uint _LBA = memoryBlock.Read32((uint)(i + 8));
                 uint _Size = memoryBlock.Read32((uint)(i + 12));
 
-                if(_Size == 0 || _LBA == 0)
+                if(_Size == 0 || _LBA == 0) 
+                {
                     continue;
+                }
 
                 PartitionInfos.Add(new PartitionInfo()
                 {
@@ -44,7 +47,12 @@ namespace Mosa.External.x86.FileSystem
                     LBA = _LBA,
                     Size = _Size,
                 });
-                Console.WriteLine("Partition: " + PartitionInfos.Count + " | Bootable: " + _IsBootable + " | LBA: " + _LBA + " | Size: " + _Size);
+            }
+
+            for (int i = 0; i < PartitionInfos.Count; i++)
+            {
+                PartitionInfo v = PartitionInfos[i];
+                //Console.WriteLine("Partition:" + i + " Bootable:" + (v.IsBootable ? "True" : "False") + " LBA:" + v.LBA + " Size:" + v.Size);
             }
         }
     }
