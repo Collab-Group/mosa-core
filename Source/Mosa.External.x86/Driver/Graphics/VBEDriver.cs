256dd3cae5540bd64a2609794d86b4b779abee60
AnErrupTion <ichicharka@gmail.com>
2021-08-01 02:03:34 +0200

    Make VBEDriver use implemented functions instead of re-impl

--- Source/Mosa.External.x86/Driver/Graphics/VBEDriver.cs
+++ Source/Mosa.External.x86/Driver/Graphics/VBEDriver.cs
@@ -1,12 +1,11 @@
-﻿using Mosa.External.x86;
-using Mosa.Kernel.x86;
-using Mosa.Runtime;
+﻿using Mosa.Kernel.x86;
 
 namespace Mosa.External.x86.Driver
 {
     public class VBEDriver
     {
-		public MemoryBlock Video_Memory;
+		public MemoryBlock VideoMemory;
+
 		public uint ScreenWidth
 		{
 			get
@@ -24,20 +23,7 @@ namespace Mosa.External.x86.Driver
 
 		public VBEDriver()
         {
-			Video_Memory = GetPhysicalMemory(VBE.MemoryPhysicalLocation, (uint)(VBE.ScreenWidth * VBE.ScreenHeight * (VBE.BitsPerPixel / 8)));
+			VideoMemory = Memory.GetPhysicalMemory(VBE.MemoryPhysicalLocation, (uint)(VBE.ScreenWidth * VBE.ScreenHeight * (VBE.BitsPerPixel / 8)));
         }
-
-		public MemoryBlock GetPhysicalMemory(Pointer address, uint size)
-		{
-			var start = (uint)address.ToInt32();
-
-			// Map physical memory space to virtual memory space
-			for (var at = start; at < start + size; at += PageFrameAllocator.PageSize)
-			{
-				PageTable.MapVirtualAddressToPhysical(at, at);
-			}
-
-			return new MemoryBlock(address, size);
-		}
 	}
 }
