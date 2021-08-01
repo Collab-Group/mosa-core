e13e1edc7b5937fb4ca35a5ee951341563f674a1
nifanfa <nifanfa@foxmail.com>
2021-08-01 16:08:29 +0800

    Update StringImpl.cs

--- Source/Mosa.External.x86/StringImpl.cs
+++ Source/Mosa.External.x86/StringImpl.cs
@@ -25,5 +25,23 @@ namespace Mosa.External.x86
             GC.DisposeObject(ls);
             return result;
         }
+
+        public static string Format(this string format, params object[] args)
+        {
+            string result = "";
+            int count = 0;
+            for (int i = 0; i < format.Length; i++)
+            {
+                if (count < 10 && format[i] == '{' && format[i + 1] == (count.ToString())[0] && format[i + 2] == '}')
+                {
+                    result += args[count].ToString();
+                    i += 3;
+                    count++;
+                    continue;
+                }
+                result += format[i];
+            }
+            return result;
+        }
     }
 }
