339ea8a7e189c2ede803885c513e6b4cf6b5257d
nifanfa <nifanfa@foxmail.com>
2021-08-06 18:09:30 +0800

    Update Run.bat

--- Source/Mosa.VisualStudio.GUI.ProjectTemplate/Properties/Run.bat
+++ Source/Mosa.VisualStudio.GUI.ProjectTemplate/Properties/Run.bat
@@ -1,3 +1,7 @@
 @echo off
-start "" "C:\Program Files (x86)\MOSA-Project\bin\Mosa.Launcher.Console.exe" "%cd%\%1%" "" False
+"C:\Program Files (x86)\MOSA-Project\bin\Mosa.Launcher.Console.exe" "%cd%\%1%" "" False
+::Add Your Commands Here
+::Add JustBuild at the end can makes the compiler not launch virtualbox after compiling
+::For more https://github.com/nifanfa/MOSA-Core/wiki
+::The work dir is /bin so you want to add your bats here you have to use cd ..\Properties
 exit
