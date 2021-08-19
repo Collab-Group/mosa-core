306cfdcc6e1e387ce69b14c6a413e55eea4bd214
AnErrupTion <ichicharka@gmail.com>
2021-08-19 19:43:50 +0200

    Implement more array and list functions

---
+++ Source/Mosa.External.x86/ArrayImpl.cs
@@ -0,0 +1,18 @@
+﻿using Mosa.Runtime;
+
+namespace Mosa.External.x86
+{
+    public static class ArrayImpl
+    {
+        public static void Reverse(this byte[] array)
+        {
+            int n = array.Length;
+            byte[] current = array;
+
+            for (int i = 0; i < n; i++)
+                array[n - 1 - i] = current[i];
+
+            GC.DisposeObject(current);
+        }
+    }
+}
---
+++ Source/Mosa.External.x86/ListImpl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Mosa.External.x86
+{
+    public static class ListImpl
+    {
+        public static List<byte> Skip(this List<byte> list, int num)
+        {
+            List<byte> newList = new List<byte>();
+
+            for (int i = num; i < list.Count; i++)
+                newList[i - num] = list[i];
+
+            return newList;
+        }
+
+        public static List<byte> Take(this List<byte> list, int num)
+        {
+            List<byte> newList = new List<byte>();
+
+            for (int i = 0; i < num; i++)
+                newList[i] = list[i];
+
+            return newList;
+        }
+    }
+}
