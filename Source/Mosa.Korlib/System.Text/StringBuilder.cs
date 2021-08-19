57615338c71e3e98bb41c87155f3c9f8a0d465e7
AnErrupTion <ichicharka@gmail.com>
2021-08-19 13:51:20 +0200

    Implement System.Text.StringBuilder

---
+++ Source/Mosa.Korlib/System.Text/StringBuilder.cs
@@ -0,0 +1,43 @@
+﻿namespace System.Text
+{
+    public class StringBuilder
+    {
+        // We could make it faster by using a char* and using GC.AllocateObject but Mosa.Korlib doesn't reference Mosa.Runtime
+        private char[] Characters = new char[int.MaxValue];
+
+        private int Length = 0;
+
+        public StringBuilder Append(char c)
+        {
+            if (Length >= Characters.Length)
+            {
+                char[] chars = new char[Length + 100];
+                Characters.CopyTo(chars, 0);
+                Characters = chars;
+            }
+
+            Characters[Length] = c;
+            Length++;
+
+            return this;
+        }
+
+        public StringBuilder AppendLine(string s)
+        {
+            for (int i = 0; i < s.length; i++)
+                Append(s[i]);
+
+            return this;
+        }
+
+        public char[] ToCharArray()
+        {
+            return Characters;
+        }
+
+        public override string ToString()
+        {
+            return new string(Characters, 0, Length);
+        }
+    }
+}
