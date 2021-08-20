d1e417cb12de66ff136c4ecf49e94a22f9c729b4
AnErrupTion <ichicharka@gmail.com>
2021-08-20 22:48:53 +0200

    Implement MemoryStream + improve Array.Reverse implementations + remove StructConverter

--- Source/Mosa.External.x86/ArrayImpl.cs
+++ Source/Mosa.External.x86/ArrayImpl.cs
@@ -4,15 +4,25 @@ namespace Mosa.External.x86
 {
     public static class ArrayImpl
     {
-        public static void Reverse(this byte[] array)
+        public static void Reverse<T>(T[] array)
         {
             int n = array.Length;
-            byte[] current = array;
+            T[] aux = array;
 
             for (int i = 0; i < n; i++)
-                array[n - 1 - i] = current[i];
+                array[n - 1 - i] = aux[i];
 
-            GC.DisposeObject(current);
+            GC.DisposeObject(aux);
+        }
+
+        public static void Reverse<T>(T[] array, int index, int length)
+        {
+            T[] aux = array;
+
+            for (int i = index; i < length; i++)
+                array[length - 1 - i] = aux[i];
+
+            GC.DisposeObject(aux);
         }
     }
 }
