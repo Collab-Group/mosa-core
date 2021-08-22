cd614dd08d01bfa641a7b0f76b493d77da8b07bb
nifanfa <nifanfa@foxmail.com>
2021-08-22 20:30:15 +0800

    Update VirtualGraphics

---
+++ Source/Mosa.External.x86/Drawing/VirtualGraphics.cs
@@ -0,0 +1,48 @@
+﻿using Mosa.Runtime.x86;
+using System.Drawing;
+
+namespace Mosa.External.x86.Drawing
+{
+    public class VirtualGraphics : Graphics
+    {
+        Image bitmap;
+
+        public VirtualGraphics(int Width, int Height)
+        {
+            bitmap = new Image(Width, Height);
+
+            Bpp = 4;
+
+            this.Width = Width;
+            this.Height = Height;
+
+            CurrentDriver = "Virtual Graphics";
+
+            VideoMemoryCacheAddr = (uint)bitmap.RawData.Address;
+
+            ResetLimit();
+        }
+
+        public override void Disable() { }
+
+        public override void Enable() { }
+
+        public override void DrawPoint(uint Color, int X, int Y)
+        {
+            if (IsInBounds(X, Y))
+                bitmap.RawData.Write32((uint)((Width * Y + X) * Bpp), Color);
+        }
+
+        public override uint GetPoint(int X, int Y)
+        {
+            if (IsInBounds(X, Y))
+                return bitmap.RawData.Read32((uint)((Width * Y + X) * Bpp));
+
+            return 0;
+        }
+
+        public override void Update()
+        {
+        }
+    }
+}
