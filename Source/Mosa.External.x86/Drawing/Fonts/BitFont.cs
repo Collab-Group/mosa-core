022a2dec266bcde2d65c48bdd118a323a79d2fe8
AnErrupTion <ichicharka@gmail.com>
2021-08-23 17:02:36 +0200

    Improve BitFont performance

--- Source/Mosa.External.x86/Drawing/Fonts/BitFont.cs
+++ Source/Mosa.External.x86/Drawing/Fonts/BitFont.cs
@@ -1,5 +1,4 @@
-﻿using Mosa.Runtime;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
 
 namespace Mosa.External.x86.Drawing.Fonts
@@ -33,82 +32,49 @@ namespace Mosa.External.x86.Drawing.Fonts
             RegisteredBitFont.Add(bitFontDescriptor);
         }
 
-        public static int DrawBitFontChar(Graphics graphics, byte[] Raw, int Size, uint color, int Index, int X, int Y, bool UseAntiAliasing, bool Calculate = false)
+        public static int DrawBitFontChar(Graphics graphics, byte[] Raw, int Size, int Size8, uint Color, byte RedOfColor, int Index, int X, int Y, bool UseAntiAliasing, bool Calculate = false)
         {
-            Color Color = System.Drawing.Color.FromArgb((int)color);
-            if (Index == -1) return Size / 2;
+            if (Index < 0)
+                return Size / 2;
 
             int MaxX = 0;
-
             bool LastPixelIsNotDrawn = false;
-
-            int SizePerFont = Size * (Size / 8);
-            byte[] Font = new byte[SizePerFont];
-
-            for (uint u = 0; u < SizePerFont; u++)
-            {
-                Font[u] = Raw[(SizePerFont * Index) + u];
-            }
+            int SizePerFont = Size * Size8 * Index;
 
             for (int h = 0; h < Size; h++)
-            {
-                for (int aw = 0; aw < Size / 8; aw++)
-                {
-
+                for (int aw = 0; aw < Size8; aw++)
                     for (int ww = 0; ww < 8; ww++)
-                    {
-                        if ((Font[(h * (Size / 8)) + aw] & (0x80 >> ww)) != 0)
+                        if ((Raw[SizePerFont + (h * Size8) + aw] & (0x80 >> ww)) != 0)
                         {
-                            if (!Calculate)
-                                graphics.DrawPoint((uint)Color.ToArgb(), X + (aw * 8) + ww, Y + h);
+                            int max = (aw * 8) + ww;
 
-                            if ((aw * 8) + ww > MaxX)
-                            {
-                                MaxX = (aw * 8) + ww;
-                            }
+                            int x = X + max;
+                            int y = Y + h;
+
+                            if (!Calculate)
+                                graphics.DrawPoint(Color, x, y);
+
+                            if (max > MaxX)
+                                MaxX = max;
 
                             if (LastPixelIsNotDrawn)
                             {
                                 if (UseAntiAliasing && !Calculate)
                                 {
-                                    int tx = X + (aw * 8) + ww - 1;
-                                    int ty = Y + h;
-                                    Color ac = Color.FromArgb((int)graphics.GetPoint(tx, ty));
-                                    byte r = (byte)(((Color.GetRed() * 127 + 127 * ac.GetRed()) >> 8) & 0xFF);
-                                    byte g = (byte)(((Color.GetRed() * 127 + 127 * ac.GetGreen()) >> 8) & 0xFF);
-                                    byte b = (byte)(((Color.GetRed() * 127 + 127 * ac.GetBlue()) >> 8) & 0xFF);
-                                    graphics.DrawPoint((uint)Color.ToArgb(ac.GetAlpha(), r, g, b), tx, ty);
+                                    Color ac = System.Drawing.Color.FromArgb((int)graphics.GetPoint(x - 1, y));
+
+                                    byte r = (byte)(((RedOfColor + (127 * ac.GetRed())) >> 8) & 0xFF);
+                                    byte g = (byte)(((RedOfColor + (127 * ac.GetGreen())) >> 8) & 0xFF);
+                                    byte b = (byte)(((RedOfColor + (127 * ac.GetBlue())) >> 8) & 0xFF);
+                                    graphics.DrawPoint((uint)System.Drawing.Color.ToArgb(ac.GetAlpha(), r, g, b), x - 1, y);
                                 }
 
                                 LastPixelIsNotDrawn = false;
                             }
                         }
-                        else
-                        {
-                            LastPixelIsNotDrawn = true;
-                        }
-                    }
-                }
-            }
-
-            GC.DisposeObject(Font);
+                        else LastPixelIsNotDrawn = true;
 
             return MaxX;
-        }
-
-        public static int Calculate(string FontName, string s)
-        {
-            foreach (var v in BitFont.RegisteredBitFont)
-                if (v.Name == FontName)
-                {
-                    int r = 0;
-                    foreach (var j in s)
-                    {
-                        r += DrawBitFontChar(null, v.Raw, v.Size, 0, v.Charset.IndexOf(j), 0, 0, false, true);
-                    }
-                    return r;
-                }
-            return 0;
         }
     }
 }
