4cec599f8ad52573fd220dda56e712e59d887f34
LeonTheDev <69083497+LeonTheDev@users.noreply.github.com>
2021-08-30 12:23:35 +0100

    Update Boot.cs

--- Source/Mosa.VisualStudio.ProjectTemplate/Boot.cs
+++ Source/Mosa.VisualStudio.ProjectTemplate/Boot.cs
@@ -14,7 +14,7 @@ namespace $safeprojectname$
         {
             // Initialize the necessary stuff
             Kernel.Setup();
-            //ACPI.Initialize();
+            //ACPI.Init();
             IDT.SetInterruptHandler(ProcessInterrupt);
 
             // Note: threads can't be created dynamically (for now)
@@ -57,7 +57,7 @@ namespace $safeprojectname$
 
                         case PS2Keyboard.KeyCode.Enter:
                             Console.WriteLine();
-                            Console.WriteLine("Input:" + Input);
+                            Console.WriteLine("Input: " + Input);
                             Input = "";
                             break;
 
