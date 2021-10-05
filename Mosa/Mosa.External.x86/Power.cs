a7c4721c158971e4c9d7da339f737bb2d75f65c5
nifanfa <nifanfa@foxmail.com>
2021-10-06 01:05:49 +0800

    Create Power.cs

---
+++ Mosa/Mosa.External.x86/Power.cs
@@ -0,0 +1,21 @@
+﻿using Mosa.External.x86.Driver;
+using Mosa.Runtime.x86;
+
+namespace Mosa.External.x86
+{
+    public class Power
+    {
+        public static void Reboot() 
+        {
+            //Use PS2 Controller To Reboot
+            while ((Native.In8(0x64) & 0x02) != 0) ;
+            Native.Out8(0x64, 0xFE);
+            Native.Hlt();
+        }
+
+        public static void Shutdown() 
+        {
+            ACPI.Shutdown();
+        }
+    }
+}
