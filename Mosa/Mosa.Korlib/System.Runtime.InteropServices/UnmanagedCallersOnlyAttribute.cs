826ee75ea1c9354abbb719e5f1f1209732cab9bb
nifanfa <nifanfa@foxmail.com>
2021-10-22 14:22:46 +0800

    Create UnmanagedCallersOnlyAttribute.cs

---
+++ Mosa/Mosa.Korlib/System.Runtime.InteropServices/UnmanagedCallersOnlyAttribute.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
+
+namespace System.Runtime.InteropServices
+{
+    public sealed class UnmanagedCallersOnlyAttribute : Attribute
+    {
+        public string EntryPoint;
+        public CallingConvention CallingConvention;
+
+        public UnmanagedCallersOnlyAttribute() { }
+    }
+}
