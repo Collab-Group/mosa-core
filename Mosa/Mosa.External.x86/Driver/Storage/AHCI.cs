d0081a1d820d2d018a9da91c69bdbdd5710d399a
LeonTheDev <69083497+LeonTheDev@users.noreply.github.com>
2021-10-26 16:30:43 +0100

    Update AHCI.cs

--- Mosa/Mosa.External.x86/Driver/Storage/AHCI.cs
+++ Mosa/Mosa.External.x86/Driver/Storage/AHCI.cs
@@ -1,4 +1,5 @@
 
+using Mosa.Kernel.x86;
 using System.Runtime.InteropServices;
 
 namespace Mosa.External.x86.Driver
@@ -44,108 +45,180 @@ namespace Mosa.External.x86.Driver
         const int VendorIntel = 0x8086;
         const int VendorVMWARE = 0x15AD;
 
-        public class tagHBA_PORT
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        public unsafe struct HBA_PORT
         {
-            public uint clb;        // 0x00, command list base address, 1K-byte aligned
-            public uint clbu;       // 0x04, command list base address upper 32 bits
-            public uint fb;         // 0x08, FIS base address, 256-byte aligned
-            public uint fbu;        // 0x0C, FIS base address upper 32 bits
-            public uint Is;         // 0x10, interrupt status
-            public uint ie;         // 0x14, interrupt enable
-            public uint cmd;        // 0x18, command and status
-            public uint rsv0;       // 0x1C, Reserved
-            public uint tfd;        // 0x20, task file data
-            public uint sig;        // 0x24, signature
-            public uint ssts;       // 0x28, SATA status (SCR0:SStatus)
-            public uint sctl;       // 0x2C, SATA control (SCR2:SControl)
-            public uint serr;       // 0x30, SATA error (SCR1:SError)
-            public uint sact;       // 0x34, SATA active (SCR3:SActive)
-            public uint ci;         // 0x38, command issue
-            public uint sntf;       // 0x3C, SATA notification (SCR4:SNotification)
-            public uint fbs;        // 0x40, FIS-based switch control
-            public uint[] rsv1 = new uint[11];   // 0x44 ~ 0x6F, Reserved
-            public uint[] vendor = new uint[4];  // 0x70 ~ 0x7F, vendor specific
+            public uint clb;
+            public uint clbu;
+            public uint fb;
+            public uint fbu;
+            public uint Is;
+            public uint ie;
+            public uint cmd;
+            public uint rsv0;
+            public uint tfd;
+            public uint sig;
+            public uint ssts;
+            public uint sctl;
+            public uint serr;
+            public uint sact;
+            public uint ci;
+            public uint sntf;
+            public uint fbs;
+            fixed uint rsv1[11];
+            fixed uint vendor[4];
         }
 
         [StructLayout(LayoutKind.Sequential, Pack = 1)]
-        public unsafe struct tagHBA_CMD_HEADER
+        public unsafe struct HBA_CMD_HEADER
         {
             public byte cflawp;
 
             public byte rbcrsv0pmp;
 
-            public uint prdtl;   // Physical region descriptor table length in entries
+            public uint prdtl;
 
-            public uint prdbc;   // Physical region descriptor byte count transferred
+            public uint prdbc;
 
-            public uint ctba;    // Command table descriptor base address
-            public uint ctbau;   // Command table descriptor base address upper 32 bits
+            public uint ctba;
+            public uint ctbau;
 
             fixed uint rsv1[4];
         }
 
         [StructLayout(LayoutKind.Sequential, Pack = 1)]
-        public unsafe struct tagFIS_REG_H2D
+        public unsafe struct FIS_REG_H2D
         {
-            public byte fis_type;  // FIS_TYPE_REG_H2D
+            public byte fis_type;
 
             public byte pmportrsv0c;
 
-            public byte command;   // Command register
-            public byte featurel;  // Feature register, 7:0
+            public byte command;
+            public byte featurel;
 
-            public byte lba0;      // LBA low register, 7:0
-            public byte lba1;      // LBA mid register, 15:8
-            public byte lba2;      // LBA high register, 23:16
-            public byte device;    // Device register
+            public byte lba0;
+            public byte lba1;
+            public byte lba2;
+            public byte device;
 
-            public byte lba3;     // LBA register, 31:24
-            public byte lba4;     // LBA register, 39:32
-            public byte lba5;     // LBA register, 47:40
-            public byte featureh; // Feature register, 15:8
+            public byte lba3;
+            public byte lba4;
+            public byte lba5;
+            public byte featureh;
 
-            public byte countl;   // Count register, 7:0
-            public byte counth;   // Count register, 15:8
-            public byte icc;      // Isochronous command completion
-            public byte control;  // Control register
+            public byte countl;
+            public byte counth;
+            public byte icc;
+            public byte control;
 
             fixed byte rsv1[4];
         }
 
         [StructLayout(LayoutKind.Sequential, Pack = 1)]
-        public struct tagHBA_PRDT_ENTRY
+        public struct HBA_PRDT_ENTRY
         {
-            public uint dba;    // Data base address
-            public uint dbau;   // Data base address upper 32 bits
-            public uint rsv0;    // Reserved
+            public uint dba;
+            public uint dbau;
+            public uint rsv0;
 
             public uint dbcrsv1i;
         }
 
         [StructLayout(LayoutKind.Sequential, Pack = 1)]
-        public unsafe struct tagHBA_MEM
+        public unsafe struct HBA_MEM
         {
-            public uint cap;     // 0x00, Host capability
-            public uint ghc;     // 0x04, Global host control
-            public uint Is;     // 0x08, Interrupt status
-            public uint pi;      // 0x0C, Port implemented
-            public uint vs;      // 0x10, Version
-            public uint ccc_ctl; // 0x14, Command completion coalescing control
-            public uint ccc_pts; // 0x18, Command completion coalescing ports
-            public uint em_loc;      // 0x1C, Enclosure management location
-            public uint em_ctl;      // 0x20, Enclosure management control
-            public uint cap2;        // 0x24, Host capabilities extended
-            public uint bohc;        // 0x28, BIOS/OS handoff control and status
+            public uint cap;
+            public uint ghc;
+            public uint Is;
+            public uint pi;
+            public uint vs;
+            public uint ccc_ctl;
+            public uint ccc_pts;
+            public uint em_loc;
+            public uint em_ctl;
+            public uint cap2;
+            public uint bohc;
 
-            // 0x2C - 0x9F, Reserved
             public fixed byte rsv[0xA0 - 0x2C];
 
-            // 0xA0 - 0xFF, Vendor specific registers
             public fixed byte vendor[0x100 - 0xA0];
 
-            // 0x100 - 0x10FF, Port control registers
-            //tagHBA_PORT	ports[1];	// 1 ~ 32
-            public fixed byte ports[0x1100 - 0x100];
+            public fixed byte ports[0x10FF - 0x100];
+        }
+
+        public const uint SATA_SIG_ATA = 0x00000101;
+        public const uint SATA_SIG_ATAPI = 0xEB140101;
+        public const uint SATA_SIG_SEMB = 0xC33C0101;
+        public const uint SATA_SIG_PM = 0x96690101;
+
+        public const byte AHCI_DEV_NULL = 0;
+        public const byte AHCI_DEV_SATA = 1;
+        public const byte AHCI_DEV_SEMB = 2;
+        public const byte AHCI_DEV_PM = 3;
+        public const byte AHCI_DEV_SATAPI = 4;
+
+        public const byte HBA_PORT_IPM_ACTIVE = 1;
+        public const byte HBA_PORT_DET_PRESENT = 3;
+
+        public unsafe void ProbePort(HBA_MEM *abar)
+        {
+            uint pi = abar->pi;
+            int i = 0;
+            while (i < 32)
+            {
+                if ((pi & 1) != 0)
+                {
+                    int dt = CheckType((HBA_PORT*)&abar->ports[i]);
+                    if (dt == AHCI_DEV_SATA)
+                    {
+                        Console.WriteLine("SATA drive found at port " + i);
+                    }
+                    else if (dt == AHCI_DEV_SATAPI)
+                    {
+                        Console.WriteLine("SATAPI drive found at port " + i);
+                    }
+                    else if (dt == AHCI_DEV_SEMB)
+                    {
+                        Console.WriteLine("SEMB drive found at port " + i);
+                    }
+                    else if (dt == AHCI_DEV_PM)
+                    {
+                        Console.WriteLine("PM drive found at port " + i);
+                    }
+                    else
+                    {
+                        Console.WriteLine("No drive found at port " + i);
+                    }
+                }
+
+                pi >>= 1;
+                i++;
+            }
+        }
+
+        public unsafe int CheckType(HBA_PORT *port)
+        {
+            uint ssts = port->ssts;
+
+            byte ipm = (byte)((ssts >> 8) & 0x0F);
+            byte det = (byte)(ssts & 0x0F);
+
+            if (det != HBA_PORT_DET_PRESENT)
+                return AHCI_DEV_NULL;
+            if (ipm != HBA_PORT_IPM_ACTIVE)
+                return AHCI_DEV_NULL;
+
+            switch (port->sig)
+            {
+                case SATA_SIG_ATAPI:
+                    return AHCI_DEV_SATAPI;
+                case SATA_SIG_SEMB:
+                    return AHCI_DEV_SEMB;
+                case SATA_SIG_PM:
+                    return AHCI_DEV_PM;
+                default:
+                    return AHCI_DEV_SATA;
+            }
         }
     }
 }
