0db4bac35f62d043ef99f8be945a0808ad2893ad
nifanfa <nifanfa@foxmail.com>
2021-10-26 13:54:42 +0800

    Update Console.cs

--- Mosa/Mosa.Kernel.x86/Console.cs
+++ Mosa/Mosa.Kernel.x86/Console.cs
@@ -5,276 +5,284 @@ using Mosa.Runtime.x86;
 
 namespace Mosa.Kernel.x86
 {
-	/// <summary>
-	/// Screen
-	/// </summary>
-	public static class Console
-	{
-		private static uint column = 0;
-		private static uint row = 0;
-		private static byte color = 0;
+    /// <summary>
+    /// Screen
+    /// </summary>
+    public static class Console
+    {
+        private static uint column = 0;
+        private static uint row = 0;
+        private static byte color = 0;
 
-		/// <summary>
-		/// The columns
-		/// </summary>
-		public const uint Columns = 80;
+        /// <summary>
+        /// The columns
+        /// </summary>
+        public const uint Columns = 80;
 
-		/// <summary>
-		/// The rows
-		/// </summary>
-		public const uint Rows = 25;
+        /// <summary>
+        /// The rows
+        /// </summary>
+        public const uint Rows = 25;
 
-		/// <summary>
-		/// Gets or sets the column.
-		/// </summary>
-		/// <value>
-		/// The column.
-		/// </value>
-		public static uint CursorLeft
-		{
-			get { return column; }
-			private set { column = value; }
-		}
+        /// <summary>
+        /// Gets or sets the column.
+        /// </summary>
+        /// <value>
+        /// The column.
+        /// </value>
+        public static uint CursorLeft
+        {
+            get { return column; }
+            private set { column = value; }
+        }
 
-		/// <summary>
-		/// Gets or sets the row.
-		/// </summary>
-		/// <value>
-		/// The row.
-		/// </value>
-		public static uint CursorTop
-		{
-			get { return row; }
-			private set { row = value; }
-		}
+        /// <summary>
+        /// Gets or sets the row.
+        /// </summary>
+        /// <value>
+        /// The row.
+        /// </value>
+        public static uint CursorTop
+        {
+            get { return row; }
+            private set { row = value; }
+        }
 
-		public static byte Color
-		{
-			get { return (byte)(color & 0x0F); }
-			set { color &= 0xF0; color |= (byte)(value & 0x0F); }
-		}
+        public static byte Color
+        {
+            get { return (byte)(color & 0x0F); }
+            set { color &= 0xF0; color |= (byte)(value & 0x0F); }
+        }
 
-		public static byte BackgroundColor
-		{
-			get { return (byte)(color >> 4); }
-			set { color &= 0x0F; color |= (byte)((value & 0x0F) << 4); }
-		}
+        public static byte BackgroundColor
+        {
+            get { return (byte)(color >> 4); }
+            set { color &= 0x0F; color |= (byte)((value & 0x0F) << 4); }
+        }
 
-		public static void Setup() 
-		{
-			BackgroundColor = ConsoleColor.Black;
-			Clear();
-			SetCursorPosition(0, 0);
-			Color = ConsoleColor.White;
-		}
+        public static void Setup()
+        {
+            BackgroundColor = ConsoleColor.Black;
+            Clear();
+            SetCursorPosition(0, 0);
+            Color = ConsoleColor.White;
+        }
 
-		/// <summary>
-		/// Next Column
-		/// </summary>
-		private static void Next()
-		{
-			CursorLeft++;
+        /// <summary>
+        /// Next Column
+        /// </summary>
+        private static void Next()
+        {
+            CursorLeft++;
 
-			if (CursorLeft >= Columns)
-			{
-				CursorLeft = 0;
-				CursorTop++;
-			}
-		}
+            if (CursorLeft >= Columns)
+            {
+                CursorLeft = 0;
+                CursorTop++;
+            }
+        }
 
-		private static void Previous()
-		{
-			if (CursorLeft == 0 && CursorTop == 0)
-			{
-				return;
-			}
+        private static void Previous()
+        {
+            if (CursorLeft == 0 && CursorTop == 0)
+            {
+                return;
+            }
 
 
-			if (CursorLeft != 0)
-			{
-				CursorLeft--;
-			}
-			else
-			{
-				CursorTop--;
-				CursorLeft = Columns - 1;
-			}
-		}
-
-		public static void RemovePreviousOne()
-		{
-			Previous();
-			UpdateCursor();
-		}
-
-		/// <summary>
-		/// Skips the specified skip.
-		/// </summary>
-		/// <param name="skip">The skip.</param>
-		private static void Skip(uint skip)
-		{
-			for (uint i = 0; i < skip; i++)
-				Next();
-		}
-
-		/// <summary>
-		/// Writes the character.
-		/// </summary>
-		/// <param name="chr">The character.</param>
-		private static void Write(char chr)
-		{
-			if (chr == '\n')
-			{
-				CursorLeft = 0;
-				CursorTop++;
-				MoveUpPrevious();
-			}
+            if (CursorLeft != 0)
+            {
+                CursorLeft--;
+            }
             else
             {
-				MoveUpPrevious();
+                CursorTop--;
+                CursorLeft = Columns - 1;
+            }
+        }
 
-				Native.Set8(0x0B8000 + ((CursorTop * Columns + CursorLeft) * 2), (byte)chr);
-				Native.Set8(0x0B8000 + ((CursorTop * Columns + CursorLeft) * 2) + 1, color);
+        private static void RemovePreviousOne()
+        {
+            Previous();
+            UpdateCursor();
+        }
 
-				Next();
-			}
+        /// <summary>
+        /// Skips the specified skip.
+        /// </summary>
+        /// <param name="skip">The skip.</param>
+        private static void Skip(uint skip)
+        {
+            for (uint i = 0; i < skip; i++)
+                Next();
+        }
 
-			UpdateCursor();
-		}
+        /// <summary>
+        /// Writes the character.
+        /// </summary>
+        /// <param name="chr">The character.</param>
+        private static void Write(char chr)
+        {
+            if (chr == '\n')
+            {
+                CursorLeft = 0;
+                CursorTop++;
+                MoveUpPrevious();
+            }
+            else
+            {
+                MoveUpPrevious();
 
-		private static void MoveUpPrevious()
-		{
-			if (CursorTop == Rows)
-			{
-				ASM.MEMCPY(0x0B8000, 0x0B80A0, 0xF00);
-				ASM.MEMFILL(0xB8F00, 0xA0, 0);
-				SetCursorPosition(0, Rows - 1);
-			}
-		}
+                Native.Set8(0x0B8000 + ((CursorTop * Columns + CursorLeft) * 2), (byte)chr);
+                Native.Set8(0x0B8000 + ((CursorTop * Columns + CursorLeft) * 2) + 1, color);
 
-		/// <summary>
-		/// Writes the string to the screen.
-		/// </summary>
-		/// <param name="value">The string value to write to the screen.</param>
-		public static void Write(string value)
-		{
-			for (int index = 0; index < value.Length; index++)
-			{
-				char chr = value[index];
-				Write(chr);
-			}
-		}
+                Next();
+            }
 
-		/// <summary>
-		/// Goto the top.
-		/// </summary>
-		private static void GotoTop()
-		{
-			CursorLeft = 0;
-			CursorTop = 0;
-			UpdateCursor();
-		}
+            UpdateCursor();
+        }
 
-		/// <summary>
-		/// Writes the line.
-		/// </summary>
-		public static void WriteLine()
-		{
-			NextLine();
-		}
+        private static void MoveUpPrevious()
+        {
+            if (CursorTop == Rows)
+            {
+                ASM.MEMCPY(0x0B8000, 0x0B80A0, 0xF00);
+                ASM.MEMFILL(0xB8F00, 0xA0, 0);
+                SetCursorPosition(0, Rows - 1);
+            }
+        }
 
-		/// <summary>
-		/// Goto the next line.
-		/// </summary>
-		private static void NextLine()
-		{
-			CursorLeft = 0;
-			CursorTop++;
-			MoveUpPrevious();
-			UpdateCursor();
-		}
+        /// <summary>
+        /// Writes the string to the screen.
+        /// </summary>
+        /// <param name="value">The string value to write to the screen.</param>
+        public static void Write(string value)
+        {
+            for (int index = 0; index < value.Length; index++)
+            {
+                char chr = value[index];
+                Write(chr);
+            }
+        }
 
-		/// <summary>
-		/// Writes the line.
-		/// </summary>
-		/// <param name="line">The line.</param>
-		public static void WriteLine(string line)
-		{
-			Write(line);
-			NextLine();
-		}
+        /// <summary>
+        /// Goto the top.
+        /// </summary>
+        private static void GotoTop()
+        {
+            CursorLeft = 0;
+            CursorTop = 0;
+            UpdateCursor();
+        }
 
-		/// <summary>
-		/// Clears this instance.
-		/// </summary>
-		public static void Clear()
-		{
-			GotoTop();
+        /// <summary>
+        /// Writes the line.
+        /// </summary>
+        public static void WriteLine()
+        {
+            NextLine();
+        }
 
-			byte c = Color;
-			Color = 0x0;
+        /// <summary>
+        /// Goto the next line.
+        /// </summary>
+        private static void NextLine()
+        {
+            CursorLeft = 0;
+            CursorTop++;
+            MoveUpPrevious();
+            UpdateCursor();
+        }
 
-			for (int i = 0; i < Columns * Rows; i++)
-				Write(' ');
+        /// <summary>
+        /// Writes the line.
+        /// </summary>
+        /// <param name="line">The line.</param>
+        public static void WriteLine(string line)
+        {
+            Write(line);
+            NextLine();
+        }
 
-			Color = c;
-			GotoTop();
-		}
+        /// <summary>
+        /// Clears this instance.
+        /// </summary>
+        public static void Clear()
+        {
+            GotoTop();
 
-		/// <summary>
-		/// Goto the specified row and column.
-		/// </summary>
-		/// <param name="row">The row.</param>
-		/// <param name="col">The col.</param>
-		public static void SetCursorPosition(uint left, uint top)
-		{
-			CursorTop = top;
-			CursorLeft = left;
-			UpdateCursor();
-		}
+            byte c = Color;
+            Color = 0x0;
 
-		/// <summary>
-		/// Sets the cursor.
-		/// </summary>
-		/// <param name="row">The row.</param>
-		/// <param name="column">The column.</param>
-		private static void SetCursor(uint row, uint column)
-		{
-			uint location = (row * Columns) + column;
+            for (int i = 0; i < Columns * Rows; i++)
+                Write(' ');
 
-			Native.Out8(0x3D4, 0x0F);
-			Native.Out8(0x3D5, (byte)(location & 0xFF));
+            Color = c;
+            GotoTop();
+        }
 
-			Native.Out8(0x3D4, 0x0E);
-			Native.Out8(0x3D5, (byte)((location >> 8) & 0xFF));
-		}
+        /// <summary>
+        /// Goto the specified row and column.
+        /// </summary>
+        /// <param name="row">The row.</param>
+        /// <param name="col">The col.</param>
+        public static void SetCursorPosition(uint left, uint top)
+        {
+            CursorTop = top;
+            CursorLeft = left;
+            UpdateCursor();
+        }
 
-		private static void UpdateCursor()
-		{
-			Native.Set8(0x0B8000 + ((CursorTop * Columns + CursorLeft) * 2), (byte)' ');
-			Native.Set8(0x0B8000 + ((CursorTop * Columns + CursorLeft) * 2) + 1, color);
-			SetCursor(CursorTop, CursorLeft);
-		}
+        /// <summary>
+        /// Sets the cursor.
+        /// </summary>
+        /// <param name="row">The row.</param>
+        /// <param name="column">The column.</param>
+        private static void SetCursor(uint row, uint column)
+        {
+            uint location = (row * Columns) + column;
 
-		public static PS2Keyboard.KeyCode ReadKey()
-		{
-			return PS2Keyboard.GetKeyPressed();
-		}
+            Native.Out8(0x3D4, 0x0F);
+            Native.Out8(0x3D5, (byte)(location & 0xFF));
 
-		public static string ReadLine()
-		{
-			string Line = "";
-			PS2Keyboard.KeyCode code;
-			while ((code = PS2Keyboard.GetKeyPressed()) != PS2Keyboard.KeyCode.Enter)
-			{
-				if (code != 0)
-				{
-					Line += code.KeyCodeToString();
-					Console.Write(code.KeyCodeToString());
-				}
-			}
-			Console.WriteLine();
-			return Line;
-		}
-	}
+            Native.Out8(0x3D4, 0x0E);
+            Native.Out8(0x3D5, (byte)((location >> 8) & 0xFF));
+        }
+
+        private static void UpdateCursor()
+        {
+            Native.Set8(0x0B8000 + ((CursorTop * Columns + CursorLeft) * 2), (byte)' ');
+            Native.Set8(0x0B8000 + ((CursorTop * Columns + CursorLeft) * 2) + 1, color);
+            SetCursor(CursorTop, CursorLeft);
+        }
+
+        public static PS2Keyboard.KeyCode ReadKey()
+        {
+            return PS2Keyboard.GetKeyPressed();
+        }
+
+        public static string ReadLine()
+        {
+            string Line = "";
+            PS2Keyboard.KeyCode code;
+            while ((code = PS2Keyboard.GetKeyPressed()) != PS2Keyboard.KeyCode.Enter)
+            {
+                if (code == PS2Keyboard.KeyCode.Delete)
+                {
+                    if (Line.Length != 0)
+                    {
+                        Console.RemovePreviousOne();
+                        Line = Line.Substring(0, Line.Length - 1);
+                    }
+                }
+                else if (code != 0)
+                {
+                    Line += code.KeyCodeToString();
+                    Console.Write(code.KeyCodeToString());
+                }
+            }
+            Console.WriteLine();
+            return Line;
+        }
+    }
 }
