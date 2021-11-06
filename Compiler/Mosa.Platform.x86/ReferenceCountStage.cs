9ad783b0909854dab057dd8bf55f22a680331304
nifanfa <nifanfa@foxmail.com>
2021-11-06 14:40:35 +0800

    Update ReferenceCountStage.cs

--- Compiler/Mosa.Platform.x86/ReferenceCountStage.cs
+++ Compiler/Mosa.Platform.x86/ReferenceCountStage.cs
@@ -1,27 +1,27 @@
 ﻿using Mosa.Compiler.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Mosa.Platform.x86
 {
     public static class ReferenceCountStage
     {
         public static void AddReferenceCount(InstructionNode node, OpcodeEncoder opcodeEncoder)
-		{
-			//if (node.Operand1.Type.Namespace.Contains("MOSA1")) 
-			if (
-				node.Operand1.Type.BaseType != null &&
-				node.Operand1.Type.BaseType.FullName == "System.Object" &&
-				node.Operand1.Type.FullName != "System.String"
-				)
-				//Debugger.Break();
-				opcodeEncoder.AppendBytes(new byte[]
-				{
-					0x83 ,0x40 ,0x04 ,0x01
-				});
-		}
+        {
+            //if (node.Operand1.Type.Namespace.Contains("MOSA1")) 
+            if (
+                !node.Operand1.IsStaticField &&
+                node.Operand1.IsReferenceType &&
+                node.Operand1.Type.BaseType != null &&
+                node.Operand1.Register != null &&
+                node.Operand1.Register.RegisterCode == 0 && //0 Is EAX Register
+                node.Operand1.Type.BaseType.FullName == "System.Object" &&
+                //TODO - String ReferenceCount
+                node.Operand1.Type.FullName != "System.String"
+                )
+                //Debugger.Break();
+                opcodeEncoder.AppendBytes(new byte[]
+                {
+                    0x83 ,0x40 ,0x04 ,0x01
+                });
+        }
     }
 }
