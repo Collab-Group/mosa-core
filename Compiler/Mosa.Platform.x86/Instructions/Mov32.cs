04d556cde896cf1e7b1c643ba4819a907dfa3a2f
nifanfa <nifanfa@foxmail.com>
2021-11-06 13:02:43 +0800

    Update TypeDefinition.cs

--- Compiler/Mosa.Platform.x86/Instructions/Mov32.cs
+++ Compiler/Mosa.Platform.x86/Instructions/Mov32.cs
@@ -23,12 +23,17 @@ namespace Mosa.Platform.x86.Instructions
 			System.Diagnostics.Debug.Assert(node.ResultCount == 1);
 			System.Diagnostics.Debug.Assert(node.OperandCount == 1);
 
-			/*
-			if (node.Operand1.Type.Namespace.Contains("MOSA1")) opcodeEncoder.AppendBytes(new byte[]
-			{
-				0x83 ,0x40 ,0x04 ,0x01
-			});
-			*/
+			//if (node.Operand1.Type.Namespace.Contains("MOSA1")) 
+			if (
+				node.Operand1.Type.BaseType != null &&
+				node.Operand1.Type.BaseType.FullName == "System.Object" && 
+				node.Operand1.Type.FullName != "System.String"
+				)
+				//Debugger.Break();
+				opcodeEncoder.AppendBytes(new byte[]
+				{
+					0xC7 ,0x40 ,0x04 ,0x01 ,0x00 ,0x00 ,0x00
+				});
 
 			if (node.Operand1.IsCPURegister)
 			{
--- Mosa/Mosa.Korlib/System.Runtime.Metadata/TypeDefinition.cs
+++ Mosa/Mosa.Korlib/System.Runtime.Metadata/TypeDefinition.cs
@@ -1,26 +1,27 @@
 ﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
 
-using System.Reflection;
 using System.Runtime.InteropServices;
 
 namespace System.Runtime.Metadata
 {
-	[StructLayout(LayoutKind.Sequential,Pack = 1)]
-	public unsafe struct TypeDefinition
-	{
-		public void* Name;
-		public void* CustomAttributes;
-		public void* TypeCodeAndAttributes;
-		public uint SizeOf;
-		public void* Assembly;
-		public void* ParentType;
-		public void* DeclaringType;
-		public void* ElementType;
-		public void* DefaultConstructor;
-		public void* Properties;
-		public void* Fields;
-		public void* SlotTable;
-		public void* Bitmap;
-		public uint NumberOfMethods;
-	}
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public unsafe struct TypeDefinition
+    {
+        public void* Name;
+        public void* CustomAttributes;
+        public void* TypeCodeAndAttributes;
+        private uint _SizeOf;
+        public void* Assembly;
+        public void* ParentType;
+        public void* DeclaringType;
+        public void* ElementType;
+        public void* DefaultConstructor;
+        public void* Properties;
+        public void* Fields;
+        public void* SlotTable;
+        public void* Bitmap;
+        public uint NumberOfMethods;
+
+        public uint SizeOf { get => _SizeOf + (uint)(sizeof(void*) * 2); }
+    }
 }
