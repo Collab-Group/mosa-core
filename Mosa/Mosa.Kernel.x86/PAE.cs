cf2fe397bee67155594509de98f195034c9bef54
nifanfa <nifanfa@foxmail.com>
2021-11-07 17:57:33 +0800

    Update PAE.cs

--- Mosa/Mosa.Kernel.x86/PAE.cs
+++ Mosa/Mosa.Kernel.x86/PAE.cs
@@ -5,7 +5,6 @@ namespace Mosa.Kernel.x86
 {
     //Physical Address Extension
     //Allow 32 bit operating systems to make use of more than 4 GB of memory.
-    //At least 64GB up to 17179869184GB(As same as 64bit operating system)
     //TODO - How?
     /*
     public static class PAE
