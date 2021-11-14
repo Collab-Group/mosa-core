0323ff6729ca1b6bfafe2f9edbf8727e1895d591
Administrator <Administrator@WIN-DIQB7H2LHJQ>
2021-11-14 16:13:27 +0800

    Revert "Bug fixes for Windows 7"
    This reverts commit cd5df01ba5f3f5a055ae064c78d73fdd3f69ecf7.

--- Compiler/Mosa.Launcher.Console/Grub2Stage.cs
+++ Compiler/Mosa.Launcher.Console/Grub2Stage.cs
@@ -1,6 +1,11 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Mosa.Launcher.Console
 {
@@ -8,10 +13,10 @@ namespace Mosa.Launcher.Console
     {
         private static void MakeISO_Grub2()
         {
-            ZipFile.ExtractToDirectory(AppFolder+ @"Tools\grub2\grub2.zip", AppFolder+ @"output\");
+            ZipFile.ExtractToDirectory(Path.Combine(AppFolder, @"Tools\grub2\grub2.zip"), Path.Combine(AppFolder, @"output\"));
 
-            File.Copy(AppFolder+ @"output\main.exe", AppFolder+ @"output\boot\main.exe", true);
-            File.Delete(AppFolder+ @"output\main.exe");
+            File.Copy(Path.Combine(AppFolder, @"output\main.exe"), Path.Combine(AppFolder, @"output\boot\main.exe"), true);
+            File.Delete(Path.Combine(AppFolder, @"output\main.exe"));
 
             //var args = $"-relaxed-filenames -J -R -o \"{ISOFilePath}\" -b isolinux.bin -no-emul-boot -boot-load-size 4 -boot-info-table \"{OutputFolder}\"";
             var args = $"-relaxed-filenames -J -R -o \"{ISOFilePath}\" -b \"{@"boot/grub/i386-pc/eltorito.img"}\" -no-emul-boot -boot-load-size 4 -boot-info-table \"{OutputFolder}\"";
