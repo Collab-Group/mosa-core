681cce762ed04e06c92e4338fbeed23d7c6080fb
nifanfa <nifanfa@foxmail.com>
2021-11-14 14:57:17 +0800

    Update GraphicsSelector.cs

--- Mosa/Mosa.External.x86/Drawing/GraphicsSelector.cs
+++ Mosa/Mosa.External.x86/Drawing/GraphicsSelector.cs
@@ -1,31 +1,27 @@
-﻿using Mosa.Kernel;
+using Mosa.Kernel;
 using Mosa.Kernel.x86;
 
 namespace Mosa.External.x86.Drawing
 {
     public static class GraphicsSelector
     {
-        VBEGraphics CurrentVBEGraphics = null;
-        VMWareSVGAIIGraphics CurrentVMWareSVGAIIGraphics = null;
+        static Graphics CurrentGraphics = null;
         
         public static Graphics GetGraphics(int width = 640, int height = 480)
         {
+            if (CurrentGraphics != null) return CurrentGraphics;
+
             //VBE Should Always Be The Top
-            if (VBE.IsVBEAvailable) {
-                
-                if(CurrentVBEGraphics == null) 
-                    CurrentVBEGraphics =  new VBEGraphics();
-                
-                return CurrentVBEGraphics;
-                
+            if (VBE.IsVBEAvailable) 
+            {
+                CurrentGraphics =  new VBEGraphics();
+                return CurrentGraphics;
             }
 
             if (PCI.Exists(VendorID.VMWare, DeviceID.SVGAIIAdapter)) {
-                
-                if(CurrentVMWareSVGAIIGraphics == null) 
-                    CurrentVMWareSVGAIIGraphics =  new VMWareSVGAIIGraphics(width, height);
-                
-                return CurrentVMWareSVGAIIGraphics 
+
+                CurrentGraphics = new VMWareSVGAIIGraphics(width, height);
+                return CurrentGraphics;
             }
 
             Panic.Error("No graphics are available for the current system.");
