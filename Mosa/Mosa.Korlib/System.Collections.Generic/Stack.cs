1c5a1e2d868875e73b0e02f3a5b5c8f36d51e44e
Elijah629 <62805599+Elijah629@users.noreply.github.com>
2021-12-19 08:55:54 -0800

    More Stack Implementation
    - Stacks can now be created from IEnumerable<T> or **IEnumerable<object>
    - Stacks can now have multitype items

--- Mosa/Mosa.Korlib/System.Collections.Generic/Stack.cs
+++ Mosa/Mosa.Korlib/System.Collections.Generic/Stack.cs
@@ -1,6 +1,6 @@
 namespace System.Collections.Generic
 {
-    public class Stack<T> : IEnumerable, IEnumerable<T>
+    public class Stack<T> : IEnumerable,IEnumerable<T>,IReadOnlyCollection<T>
     {
         private List<T> _array;
 
@@ -21,12 +21,44 @@ namespace System.Collections.Generic
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
         public void Push(T item) => _array.Add(item);
         public T Peek() => _array[_array.Count - 1];
-        public T Pop() {
+        public T Pop()
+        {
             var value = _array[_array.Count - 1];
             _array.RemoveAt(_array.Count - 1);
             return value;
         }
-        public bool Contains(T item) => _array.Contains(item); 
+        public bool Contains(T item) => _array.Contains(item);
         public T[] ToArray() => _array.ToArray();
+    }
+    public class Stack : IEnumerable<object>, IEnumerable, IReadOnlyCollection<object>
+    {
+        private List<object> _array;
+
+        public Stack() => _array = new List<object>();
+        public Stack(int capacity)
+        {
+            if (capacity < 0)
+                throw new ArgumentOutOfRangeException(nameof(capacity), capacity.ToString());
+            _array = new List<object>(capacity);
+        }
+        public int Count => _array.Count;
+        public void Clear() => _array = new List<object>(_array.Capacity);
+        public IEnumerator<object> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+                yield return _array[i];
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        
+        public void Push(object item) => _array.Add(item);
+        public object Peek() => _array[_array.Count - 1];
+        public object Pop()
+        {
+            var value = _array[_array.Count - 1];
+            _array.RemoveAt(_array.Count - 1);
+            return value;
+        }
+        public bool Contains(object item) => _array.Contains(item);
+        public object[] ToArray() => _array.ToArray();
     }
 }
