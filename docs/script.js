37929e7a4e5457af216ddf20418bcd627aa67c52
Elijah629 <62805599+Elijah629@users.noreply.github.com>
2021-12-20 15:59:06 -0800

    script

--- docs/script.js
+++ docs/script.js
@@ -1,8 +1,13 @@
 $("#site").hide(0, () => $(() => new Promise(e => {
     // preload
-    fetch("./articles/index.json").then(x => x.json()).then(x => console.log(x));
+    fetch("./articles/index.json").then(x => x.json()).then(x => x.forEach(article => {
+        console.log(article.name);
+    }));
     e();
 }).then(() => {
     // afterload (don't use)
     $("#loader").fadeOut("slow", () => $("#site").show());
 })));
+var loadArticle = (url) => {
+
+}
