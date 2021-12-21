3321575e07940d799fbf5aa0ca5cddd8b58c6847
Elijah629 <62805599+Elijah629@users.noreply.github.com>
2021-12-21 10:20:18 -0800

    CopyBTN

--- docs/script.js
+++ docs/script.js
@@ -14,29 +14,22 @@ $("#site").hide(0, () => $(() => new Promise(e => {
           {
             type: 'output',
             filter: function (text, converter, options) {
-              // use new shodown's regexp engine to conditionally parse codeblocks
-              var left  = '<pre><code\\b[^>]*>',
-                  right = '</code></pre>',
-                  flags = 'g',
-                  replacement = function (wholeMatch, match, left, right) {
-                    // unescape match to prevent double escaping
-                    match = htmlunencode(match);
-                    return left + hljs.highlightAuto(match).value + right;
-                  };
-              return showdown.helper.replaceRecursiveRegExp(text, replacement, left, right, flags);
+              return showdown.helper.replaceRecursiveRegExp(text, function (wholeMatch, match, left, right) {
+                match = htmlunencode(match);
+                return `<pre><code\\b[^>]*><button class="copybtn" onclick="navigator.clipboard.writeText('${match}')"></button>${hljs.highlightAuto(match).value}</code></pre>`;
+              }, '<pre><code\\b[^>]*>', '</code></pre>', 'g');
             }
           }
         ];
       }),
       MDConverter = new showdown.Converter({
         extensions: ["highlightjs"],
-        //omitExtraWLInCodeBlocks: !0,
-        //noHeaderId: !0,
-        //parseImgDimensions: !0,
-        //strikethrough: !0,
-        //requireSpaceBeforeHeadingText: !0,
-        //emoji: !0,
-        //openLinksInNewWindow: !0
+        omitExtraWLInCodeBlocks: !0,
+        noHeaderId: !0,
+        parseImgDimensions: !0,
+        strikethrough: !0,
+        emoji: !0,
+        openLinksInNewWindow: !0
       }),
       MDConverter.setFlavor('github');
       loadArticle('README.md');
@@ -48,7 +41,6 @@ $("#site").hide(0, () => $(() => new Promise(e => {
           });
         } else {
             var $el = $(`<li class="catagory">${catagoryName}<br><ul></ul></li>`);
-            
             catagoryArticles.forEach((article,i,a) => {
                 $($el[0].children[1]).append(`<li class=\"article\" onclick=\"loadArticle(\'${article.filepath}\')\">${article.name}</li>`)
                 if (i == a.length - 1) {
@@ -58,12 +50,4 @@ $("#site").hide(0, () => $(() => new Promise(e => {
         }
     })).then(e);
 }).then(() => $("#loader").fadeOut("slow", () => $("#site").show()))));
-var loadArticle = (url) => {
-    fetch(`./articles/${url}`).then(x => {
-      return x.ok ? x : !function() { throw new Error("Invalid filepath") }();
-    }).then(x => x.text()).then(x => {
-        $(".Content").html(MDConverter.makeHtml(x)/*.replace(/<code>(.*)<\/code>/g, (e, o) => `<pre><code>${hljs.highlightAuto(o).value}</code></pre>`)*/);
-    }).catch(x => {
-      alert(x);
-    });
-}
+var loadArticle = (url) => fetch(`./articles/${url}`).then(x => x.ok ? x : !function() { throw new Error("Invalid filepath") }()).then(x => x.text()).then(x => $(".Content").html(MDConverter.makeHtml(x))).catch(x => alert(x));
