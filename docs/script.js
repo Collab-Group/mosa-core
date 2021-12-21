94c0a959e65dc86f6eba8138d0564647c7fcf8cc
Elijah629 <62805599+Elijah629@users.noreply.github.com>
2021-12-20 16:39:48 -0800

    yo

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,32 @@
+var MDConverter;
+
 $("#site").hide(0, () => $(() => new Promise(e => {
-    // preload
+    showdown.extension("font", () => [{
+        type: "output",
+        filter: e => e.replace(/\$\{(.+)\}\((.+)\)/g, (e, o, a) => `<span style="font-family: ${o};">${a}</span>`)
+      }]), showdown.extension("webpage", () => [{
+        type: "output",
+        filter: e => e.replace(/~\{(.+)\}\((\S+|(\S+) =(\d+)x(\d+))\)/g, (_, o, a, t, r, s) => `<iframe src="${t||a}" style="border:0" ${r?`width=${r}`:""} ${s?`height=${s}`:""} title="${o}">${o}</iframe>`)
+      }]), showdown.extension("message", () => [{
+        type: "output",
+        filter: e => e.replace(/<\/?p[^>]*>/g, "")
+      }]), showdown.extension("highlightjs", () => [{
+        type: "output",
+        filter: e => e.replace(/<code>(.*)<\/code>/g, (e, o) => `<pre><code>${hljs.highlightAuto(o).value}</code></pre>`)
+      }]);
+      MDConverter = new showdown.Converter({
+        extensions: ["message", "font", "webpage", "highlightjs"],
+        omitExtraWLInCodeBlocks: !0,
+        noHeaderId: !0,
+        parseImgDimensions: !0,
+        strikethrough: !0,
+        requireSpaceBeforeHeadingText: !0,
+        emoji: !0,
+        openLinksInNewWindow: !0
+      });
     fetch("./articles/index.json").then(x => x.json()).then(x => x.forEach(article => {
         console.log(article.name);
+        $("#articles").append(`<li onclick="${loadArticle(article.filename)}">${article.name}</li>`);
     }));
     e();
 }).then(() => {
@@ -9,5 +34,7 @@ $("#site").hide(0, () => $(() => new Promise(e => {
     $("#loader").fadeOut("slow", () => $("#site").show());
 })));
 var loadArticle = (url) => {
-
+    fetch(url).then(x => x.text()).then(x => {
+        $(".Content").html(MDConverter.makeHtml(x));
+    });
 }
